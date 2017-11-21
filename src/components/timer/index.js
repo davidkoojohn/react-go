546250e225c37cc46b3d38603ae4241605f889bc
davidkoojohn <davidkoojohn@gmail.com>
2017-11-21 21:33:21 +0800

    add md component

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Timer from './timer'
 import Todo from './todo'
+import MD from './md'
 
 class TimerApp extends Component {
   render () {
@@ -8,6 +9,7 @@ class TimerApp extends Component {
       <div className="timer-app">
         <Timer/>
         <Todo/>
+        <MD/>
       </div>
     )
   }
---
+++ src/components/timer/md.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class MD extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Type some *markdown* here!' };
+  }
+
+  handleChange (e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  getRawMarkup () {
+    //const md = new Remarkable();
+    //return {__html: md.render(this.state.value)}
+    return {__html: this.state.value}
+  }
+
+  render() {
+    return (
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <textarea onChange={this.handleChange}
+                  defaultValue={this.state.value} />
+        <h3>Output</h3>
+        <div className="content"
+             dangerouslySetInnerHTML={this.getRawMarkup()}/>
+      </div>
+    );
+  }
+}
+
+export default MD
