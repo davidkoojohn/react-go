82acdb037f87790235f40e9f0c8964bfa2b40ea6
davidkoojohn <davidkoojohn@gmail.com>
2017-11-21 16:38:48 +0800

    refactor dir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/style/index.css';
 import Hello from './components/hello';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './startup/registerServiceWorker';
 
 class App extends React.Component {
   render () {
