8d71a9f4ef5acc633af59945beffde941a0e26a2
davidkoojohn <davidkoojohn@gmail.com>
2018-12-25 23:38:54 +0800

    chore: build demo

---
+++ build/precache-manifest.a600fa6192fcd6fe9cd161fb7926995b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4bd2eb48bc79ce71ea7a",
+    "url": "/react-website/static/js/runtime~main.4bd2eb48.js"
+  },
+  {
+    "revision": "532b632834071d860b55",
+    "url": "/react-website/static/js/main.532b6328.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "532b632834071d860b55",
+    "url": "/react-website/static/css/main.33e8d170.chunk.css"
+  },
+  {
+    "revision": "8fad86dc4def2c424af8db8589f6b4e4",
+    "url": "/react-website/index.html"
+  }
+];
